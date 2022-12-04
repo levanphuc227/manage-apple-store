72656d65f193f475fea0cb52dac869d0b21dcf22
DESKTOP-KBPUDJV\admin <phuc112112112@gmail.com>
2022-12-04 10:43:21 +0700

    add env for http request

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const httpRequest = axios.create({
     // baseURL: process.env.REACT_APP_BASE_URL,
-    baseURL: 'https://api.levanphuc.asia',
+    baseURL: process.env.REACT_APP_API_URL_BASE,
 });
 
 export default httpRequest;
